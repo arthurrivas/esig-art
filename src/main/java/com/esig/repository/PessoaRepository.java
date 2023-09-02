879042056826693bc5927aa80c871ca5459baaa5
arthur rivas <arthurrivas1@gmail.com>
2023-09-02 11:56:55 -0300

    repository pessoa

---
+++ src/main/java/com/esig/repository/PessoaRepository.java
@@ -0,0 +1,31 @@
+package com.esig.repository;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import com.esig.model.Pessoa;
+
+public class PessoaRepository {
+	
+	@Inject
+	private EntityManager manager;
+
+	public PessoaRepository() {}
+	
+	
+	public List<Pessoa> buscarTodasPessoas() {
+        CriteriaBuilder cb = manager.getCriteriaBuilder();
+        CriteriaQuery<Pessoa> query = cb.createQuery(Pessoa.class);
+        Root<Pessoa> root = query.from(Pessoa.class);
+
+        query.select(root);
+
+        return manager.createQuery(query).getResultList();
+    }
+	
+}
