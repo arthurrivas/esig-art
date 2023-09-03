31cd07140579046fe2bbe5ff0d0d81a0e0fcedbb
arthur rivas <arthurrivas1@gmail.com>
2023-09-03 00:03:35 -0300

    nova entity

---
+++ src/main/java/com/esig/model/PessoaSalario.java
@@ -0,0 +1,75 @@
+package com.esig.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_salario")
+public class PessoaSalario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE )
+	private Long id;
+	
+	@ManyToOne
+    @JoinColumn(name = "pessoa_id")
+    private Pessoa pessoa;
+	private BigDecimal salario;
+	@Column(name = "created_at")
+	private Date createdAt;
+	
+	
+
+	public PessoaSalario() {}
+	
+	public PessoaSalario(Long id, Pessoa pessoa, BigDecimal salario, Date createdAt) {
+		super();
+		this.id = id;
+		this.pessoa = pessoa;
+		this.salario = salario;
+		this.createdAt = createdAt;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public BigDecimal getSalario() {
+		return salario;
+	}
+
+	public void setSalario(BigDecimal salario) {
+		this.salario = salario;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	
+}
