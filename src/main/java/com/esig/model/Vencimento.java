12e457eb1f3ece3757f96e6afe667485330d3e1c
arthur rivas <arthurrivas1@gmail.com>
2023-09-02 23:59:24 -0300

    entity vencimento

--- src/main/java/com/esig/model/Vencimento.java
+++ src/main/java/com/esig/model/Vencimento.java
@@ -18,8 +18,53 @@ public class Vencimento {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Integer id;
 	
+	private String descricao ;
 	private BigDecimal valor;
 	
-	@Enumerated(EnumType.STRING) // Especifica que o atributo é um enum
+	@Enumerated(EnumType.STRING)
     private TipoVencimento tipo;
+
+	public Vencimento() {}
+	
+	public Vencimento(Integer id, String descricao, BigDecimal valor, TipoVencimento tipo) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+		this.valor = valor;
+		this.tipo = tipo;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public TipoVencimento getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoVencimento tipo) {
+		this.tipo = tipo;
+	}
+	
+	
 }
