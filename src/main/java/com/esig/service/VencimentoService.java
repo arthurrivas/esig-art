63177b3bf59c961a78fa5f5f984cc6a4cf3fde45
arthur rivas <arthurrivas1@gmail.com>
2023-09-03 09:33:35 -0300

    funcao calcular salario usando vencimentos

---
+++ src/main/java/com/esig/service/VencimentoService.java
@@ -0,0 +1,24 @@
+package com.esig.service;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+import com.esig.model.TipoVencimento;
+import com.esig.model.Vencimento;
+
+public class VencimentoService {
+
+	
+	public BigDecimal calcularSalarioVencimentos(Set<Vencimento> vencimentos) {
+		BigDecimal salario = new BigDecimal(0);
+		
+		for(Vencimento vencimento : vencimentos) {
+			TipoVencimento tipo = vencimento.getTipo();
+			
+			if(tipo == TipoVencimento.DEBITO) salario = salario.subtract(vencimento.getValor());
+			else if(tipo == TipoVencimento.CREDITO) salario = salario.add(vencimento.getValor()); 
+			
+		}
+		return salario;
+	}
+}
