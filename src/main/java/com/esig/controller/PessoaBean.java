a7154a429badac03d36c1e0edbc8a19981f57bbe
arthur rivas <arthurrivas1@gmail.com>
2023-09-07 13:42:32 -0300

    adicionado edicao pessoa

--- src/main/java/com/esig/controller/PessoaBean.java
+++ src/main/java/com/esig/controller/PessoaBean.java
@@ -1,27 +1,37 @@
 package com.esig.controller;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.esig.model.Cargo;
 import com.esig.model.Pessoa;
 import com.esig.model.PessoaSalario;
+import com.esig.service.CargoService;
 import com.esig.service.PessoaService;
 
 @Named
 @ManagedBean(name="pessoaBean")
-@RequestScoped
-public class PessoaBean {
+@ViewScoped
+public class PessoaBean implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
 	@Inject
 	private PessoaService pessoaService;
+	@Inject
+	private CargoService cargoService;
+	private CargoConverter cargoConverter;	
 	
+	private Pessoa pessoa;
 	private List<Pessoa> listaPessoas = new ArrayList<>();
+	private List<Cargo> listaCargos = new ArrayList<>();
 	private String termoPesquisa;
 	
 	public PessoaBean(){
@@ -29,8 +39,17 @@ public class PessoaBean {
 	
 	@PostConstruct
 	public void onCreated() {
-		buscaPessoa();
-	}		
+		buscaCargos();
+		cargoConverter = new CargoConverter(getListaCargos());
+	}
+	
+	public void salvarPessoa() {
+		pessoaService.salvar(pessoa);
+	}
+	
+	public void resetPessoa() {
+		pessoa = new Pessoa();
+	}
 	
 	public void buscaPessoa() {
 		if ((termoPesquisa != null && !termoPesquisa.isEmpty())) {
@@ -48,6 +67,10 @@ public class PessoaBean {
 		setListaPessoas(pessoaService.buscaPorNome(termoPesquisa));
 	}
 	
+	public void buscaCargos() {
+		setListaCargos(cargoService.buscaCargos());		
+	}
+	
 	public void clicadoCalculaTodosSalarios() {	
 		pessoaService.calcularTodosSalarios();
 		buscaPessoaPorNome();
@@ -55,6 +78,19 @@ public class PessoaBean {
 	
 	public PessoaSalario getUltimoSalario(List<PessoaSalario> salarios) {
 		return pessoaService.getUltimoSalario(salarios);
+	}
+	
+	public boolean selecionadoPessoaEdicao() {
+		return (pessoa != null || pessoa.getId() != null);
+	}
+	
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
 	}
 
 	public List<Pessoa> getListaPessoas() {
@@ -65,6 +101,14 @@ public class PessoaBean {
 		this.listaPessoas = listaPessoas;
 	}
 
+	public List<Cargo> getListaCargos() {
+		return listaCargos;
+	}
+
+	public void setListaCargos(List<Cargo> listaCargos) {
+		this.listaCargos = listaCargos;
+	}
+
 	public String getTermoPesquisa() {
 		return termoPesquisa;
 	}
@@ -72,4 +116,13 @@ public class PessoaBean {
 	public void setTermoPesquisa(String termoBusca) {
 		this.termoPesquisa = termoBusca;
 	}
+
+	public CargoConverter getCargoConverter() {
+		return cargoConverter;
+	}
+
+	public void setCargoConverter(CargoConverter cargoConverter) {
+		this.cargoConverter = cargoConverter;
+	}
+	
 }
